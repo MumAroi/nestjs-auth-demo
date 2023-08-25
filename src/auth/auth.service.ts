5469cee926ac80562281aea7f21ed807a6e6f0ad
Paramas <paramas.w@livinginsider.com>
2023-08-25 15:27:07 +0700

    feat: ✨ signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as bcrypt from "bcrypt";
@@ -7,6 +7,48 @@ import { JwtService } from "@nestjs/jwt";
 @Injectable()
 export class AuthService {
 	constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+	async signupLocal(dto: AuthDto): Promise<Tokens> {
+		const hash = await this.hashData(dto.password);
+		const newUser = await this.prisma.user.create({
+			data: {
+				email: dto.email,
+				hash,
+			},
+		});
+		const tokens = await this.getTokens(newUser.id, newUser.email);
+		await this.updateRtHash(newUser.id, tokens.refresh_token);
+		return tokens;
+	}
+
+	async signinLocal(dto: AuthDto): Promise<Tokens> {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email,
+				deletedAt: null
+			},
+		});
+		if (!user) throw new ForbiddenException("Access Denied");
+		const passwordMatches = await bcrypt.compare(dto.password, user.hash);
+		if (!passwordMatches) throw new ForbiddenException("Access Denied");
+		const tokens = await this.getTokens(user.id, user.email);
+		await this.updateRtHash(user.id, tokens.refresh_token);
+		return tokens;
+	}
+	logout() {}
+	refreshTokens() {}
+
+	async updateRtHash(userId: number, rt: string) {
+		const hash = await this.hashData(rt);
+		await this.prisma.user.update({
+			where: {
+				id: userId,
+			},
+			data: {
+				hashedRt: hash,
+			},
+		});
+	}
 
 	hashData(data: string) {
 		return bcrypt.hash(data, 10);
@@ -41,19 +83,4 @@ export class AuthService {
 			refresh_token: rt,
 		};
 	}
-
-	async signupLocal(dto: AuthDto): Promise<Tokens> {
-		const hash = await this.hashData(dto.password);
-		const newUser = await this.prisma.user.create({
-			data: {
-				email: dto.email,
-				hash,
-			},
-		});
-		const token = await this.getTokens(newUser.id, newUser.email);
-		return token;
-	}
-	signinLocal() {}
-	logout() {}
-	refreshTokens() {}
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,10 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 export class AuthDto {
 	@IsNotEmpty()
-	@IsString()
-	email: string;
-
-	@IsNotEmpty()
 	@IsEmail()
+	email: string;
+	
+	@IsNotEmpty()
+	@IsString()
 	password: string;
 }
