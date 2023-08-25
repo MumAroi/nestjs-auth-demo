dcca41a58c3fae079cdd102e74d29141e3c8a27c
Paramas <paramas.w@livinginsider.com>
2023-08-25 11:57:29 +0700

    feat: ✨ jwt at and rt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { AtStrategy, RtStrategy } from "./strategies";
+import { JwtModule } from "@nestjs/jwt";
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+	imports: [JwtModule.register({})],
+	controllers: [AuthController],
+	providers: [AuthService, AtStrategy, RtStrategy],
 })
 export class AuthModule {}
